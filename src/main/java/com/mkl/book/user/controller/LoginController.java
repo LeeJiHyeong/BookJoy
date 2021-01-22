a72b84e49f40929dd54fbab6916ffea88d90ded2
LeeJiHyeong <mky1168@naver.com>
2021-01-23 00:57:01 +0900

    [ADD] UserReposiroty 의존성 주입

--- src/main/java/com/mkl/book/user/controller/LoginController.java
+++ src/main/java/com/mkl/book/user/controller/LoginController.java
@@ -1,7 +1,19 @@
 package com.mkl.book.user.controller;
 
+import com.mkl.book.user.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/login")
 public class LoginController {
+    private UserRepository userRepository;
+
+    @Autowired
+    public LoginController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
 }

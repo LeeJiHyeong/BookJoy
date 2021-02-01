5783c33ff24fb254804de8ee07c36edba378de2c
LeeJiHyeong <mky1168@naver.com>
2021-02-01 23:17:08 +0900

    [REFACTOR] user 테이블 칼럼 변경

--- src/main/resources/book_joy_sql.sql
+++ src/main/resources/book_joy_sql.sql
@@ -1,9 +1,10 @@
 CREATE TABLE user (
 	id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     user_email VARCHAR(70) NOT NULL UNIQUE,
-    user_password VARCHAR(70) NOT NULL,
-    user_nickname VARCHAR(30) NOT NULL UNIQUE,
-    create_date DATETIME
+    user_nickname VARCHAR(30),
+    user_picture VARCHAR(1000),
+    user_role VARCHAR(10),
+    create_date DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE Book (

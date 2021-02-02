f4ad9b5b52b330df85c2a7f250065f3262d8a2a6
tmdrl5779 <tmdrl5779@gmail.com>
2021-02-02 21:24:11 +0900

    [REFACTOR] Book 테이블 칼럼 변경

--- src/main/resources/book_joy_sql.sql
+++ src/main/resources/book_joy_sql.sql
@@ -10,12 +10,12 @@ CREATE TABLE user (
 CREATE TABLE Book (
     item_id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     isbn VARCHAR(30) NOT NULL UNIQUE,
-    title VARCHAR(100) NOT NULL,
+    title VARCHAR(200) NOT NULL,
     author VARCHAR(100) NOT NULL,
     description VARCHAR(2000),
     pub_date VARCHAR(30),
-    cover_small_url VARCHAR(100) UNIQUE,
-    cover_large_url VARCHAR(100) UNIQUE,
+    cover_small_url VARCHAR(100),
+    cover_large_url VARCHAR(100),
     category_name VARCHAR(10),
     category_id VARCHAR(10),
     ranking INTEGER

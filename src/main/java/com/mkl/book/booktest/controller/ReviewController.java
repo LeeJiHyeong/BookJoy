9cedb0cc67aae253632bbc494dcf40da64b4c23c
tmdrl5779 <tmdrl5779@gmail.com>
2021-02-04 20:44:45 +0900

    [ADD] Review 작성 테스트 코드

---
+++ src/main/java/com/mkl/book/booktest/controller/ReviewController.java
@@ -0,0 +1,28 @@
+package com.mkl.book.booktest.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/review")
+public class ReviewController {
+
+    @GetMapping("/list")
+    public String ReviewList(){
+        return "ReviewListTest";
+    }
+
+    @GetMapping("/detail")
+    public String ReviewDetail(@RequestParam("id") String id){
+        return "ReviewDetailTest"; //해당 책id의 페이지가 나와야함
+    }
+
+    @PostMapping("/detail")
+    public void ReviewInput(String review){
+
+
+    }
+}
